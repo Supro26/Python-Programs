ec311397a41ed34691078fb5c31a858edfa82a13
Supro26 <suprojitmallick2006@gmail.com>
2025-09-24 22:29:37 +0530

    updated

--- Tools/passCrack.py
+++ Tools/passCrack.py
@@ -1,19 +1,16 @@
-import sys
 import hashlib # library to hash passwords
-with open('/home/bokuto/Downloads/10-million-password-list-top-100.txt','r') as f:
+
+with open('/***/****/**/10-million-password-list-top-100.txt','r') as f:   #opening password_list file
     passwords=f.read().splitlines()
-# print(passwords)
+
 uhlist=[]
 with open('usrname&hash.txt','r') as u:
     username=u.read().splitlines()
     for a in username:
         usrhash=a.split(":")
         uhlist.append(usrhash)
-uh2=uhlist
-# pas = "summer"
-# hash= hashlib.sha256(pas.encode('utf-8'))
-# print(hash.hexdigest())
-for a in uh2:
+
+for a in uhlist:
     for pas in passwords:   
         hash= hashlib.sha256(pas.encode('utf-8')).hexdigest()
         if(hash==a[1]):
